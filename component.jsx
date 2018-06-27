84adb1407396eaf5ec3f2dc924c0c9939f613fd2
Mile <mcabrerapatino@gmail.com>
2018-06-26 19:53:10 -0500

    Server was created

---
+++ component.jsx
@@ -0,0 +1,11 @@
+const React = require('react')
+
+class App extends React.Component{
+    render() {
+        return (
+            React.createElement('h1', null, 'COmponente funcionando')
+        )
+    }
+}
+
+module.exports = App
---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.get('/',(req,res) => {
+    res.send('Servidor funcionando')
+})
+
+app.listen(3000, () => {
+    console.log('server on port 3000')
+})
